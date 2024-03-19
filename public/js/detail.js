9181554571c183ff40561bf176c37c29832be325
유안 <euantheuan@google.com>
2024-03-19 15:10:55 +0900

    rereply deleted, once dev is finished then will  upload

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,6 +3,13 @@ const storage = firebase.storage();
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
+        $("button.back").click(()=>{
+            window.location.href = 'board.html'
+        })
+        $("button.upload").click(()=>{
+            window.location.href = 'uploadboard.html'
+        })
+
         let queryString = new URLSearchParams(window.location.search)
 
         db.collection('board').doc(queryString.get('id')).get().then((result) => {
@@ -30,9 +37,10 @@ firebase.auth().onAuthStateChanged(function(user) {
                             </div>
                             <div class='replyInput'>
                                 <textarea class="form-control" rows="2"></textarea>
-                                <button type="button" id="btnReply" class="btn btn-warning">답글 달기</button>
+                                <button type="button" id="btnReply" class="btn btn-warning">댓글 달기</button>
                             </div>
                         </div>`;
+
                     $('.wrapper').append(post)
                 } else {
                     let post = `
@@ -56,6 +64,8 @@ firebase.auth().onAuthStateChanged(function(user) {
                             `;
                     $('.wrapper').append(post)
                 }
+
+                
                 
                 if (result.data().uid == user.uid) {
                     let btn = `
@@ -69,170 +79,82 @@ firebase.auth().onAuthStateChanged(function(user) {
                     })
                 }
 
-            // === 여기까지 메인 포스트 ===
-
                 const mainPostId = result.id
                 const checkReplies = function() {
-                    const repliesRef = db.collection('board').doc(mainPostId).collection('replies');
-                    repliesRef.orderBy("date", "asc").onSnapshot((snapshot) => {
-                        if (snapshot.size > 0) {
-                            snapshot.forEach((doc) => {
-                                const timestamp = doc.data().date; 
-                                const date = timestamp.toDate();
-                                const [year, month, day, hour, min, sec] = [
-                                                                            date.getFullYear(),
-                                                                            ('0'+(date.getMonth() + 1)).slice(-2),
-                                                                            ('0' + date.getDate()).slice(-2),
-                                                                            ('0' + date.getHours()).slice(-2),
-                                                                            ('0' + date.getMinutes()).slice(-2),
-                                                                            ('0' + date.getSeconds()).slice(-2)
-                                                                            ];
-                                //댓글 본문 밑에 보여주기
-                                let reply = `
-                                    <div class="thisreply">
-                                        <div class="reply">
-                                            <div class="reply_content">
-                                                <p class="content_reply">${doc.data().content}</p>
-                                                <p class="writer_reply">${doc.data().writer}</p>
-                                                <p class="date_reply">${year}-${month}-${day} ${hour}:${min}:${sec}</p>
-                                            </div>
-                                        </div>
-                                        <div class='replyOrDelete'>
-                                            <textarea class="form-control rereplyInput" rows="1"></textarea>
-                                            <button class='rereply' data-id="${doc.id}">답글</button>
-                                        </div>
-                                        <div class='rereply_area'></div>
-                                    </div>`;
-                                const mainPost = document.querySelector('.mainPost')
-                                mainPost.insertAdjacentHTML('afterend', reply)
+                const repliesRef = db.collection('board').doc(mainPostId).collection('replies');
+            
+                repliesRef.orderBy("date", "asc").onSnapshot((snapshot) => {
+                    if (snapshot.size > 0) {
+                        snapshot.forEach((doc) => {
+                            const timestamp = doc.data().date; 
+                            const date = timestamp.toDate();
+                            const [year, month, day, hour, min, sec] = [
+                                                                        date.getFullYear(),
+                                                                        ('0'+(date.getMonth() + 1)).slice(-2),
+                                                                        ('0' + date.getDate()).slice(-2),
+                                                                        ('0' + date.getHours()).slice(-2),
+                                                                        ('0' + date.getMinutes()).slice(-2),
+                                                                        ('0' + date.getSeconds()).slice(-2)
+                                                                        ];
+                            //댓글 본문 밑에 보여주기
+                            let reply = `
+                            <div class="thisreply">
+                                <div class="reply">
+                                    <div class="reply_content">
+                                        <p class="content_reply">${doc.data().content}</p>
+                                        <p class="writer_reply">${doc.data().writer}</p>
+                                        <p class="date_reply">${year}-${month}-${day} ${hour}:${min}:${sec}</p>
+                                    </div>
+                                </div>
+                                <!--<div class='replyOrDelete'>
+                                    <textarea class="form-control rereplyInput" rows="1"></textarea>
+                                    <button class='rereply' data-id="${doc.id}">답글</button>
+                                </div>
+                                <div class='rereply_area'></div>-->
+                            </div>`;
+                            const mainPost = document.querySelector('.mainPost')
+                            mainPost.insertAdjacentHTML('afterend', reply)
 
-                                if (doc.data().uid == user.uid) {
-                                    const btnDeleteReply = `<button class="btnDeleteReply" data-id="${doc.id}">삭제</button>`
-                                    document.querySelector('.reply').insertAdjacentHTML('beforeend', btnDeleteReply)
+                            if (doc.data().uid == user.uid) {
+                                const btnDeleteReply = `<button class="btnDeleteReply" data-id="${doc.id}">삭제</button>`
+                                document.querySelector('.thisreply').insertAdjacentHTML('beforeend', btnDeleteReply)
 
-                                    $(".btnDeleteReply").click(function() {
-                                        let docId = $(this).data('id');
-                        
-                                        if (confirm('삭제하시겠습니까?')) {
-                                            db.collection('board').doc(mainPostId).collection('replies').doc(docId).delete().then(() => {
-                                                alert('삭제되었습니다.')
-                                                location.reload(true);
-                                            }).catch((error) => {
-                                                console.log('게시글 삭제 중 에러가 발생했습니다:', error);
-                                                alert('게시글 삭제 중 에러가 발생했습니다.')
-                                            });
-                                        }
-                                    })
-                                }
-
-                            // --- 대댓글 확인 ---
-                                const checkRereplies = function() {
-                                    const rereplyRef = db.collection('board').doc(mainPostId).collection('replies').doc(doc.id).collection('rereplies')
-
-                                        rereplyRef.orderBy("date", "asc").onSnapshot((snapshot) => {
-
-                                        if (snapshot.size > 0) {
-                                            snapshot.forEach((doc) => {
-                                                const timestamp = doc.data().date; 
-                                                const date = timestamp.toDate();
-                                                const [year, month, day, hour, min, sec] = [
-                                                                                            date.getFullYear(),
-                                                                                            ('0'+(date.getMonth() + 1)).slice(-2),
-                                                                                            ('0' + date.getDate()).slice(-2),
-                                                                                            ('0' + date.getHours()).slice(-2),
-                                                                                            ('0' + date.getMinutes()).slice(-2),
-                                                                                            ('0' + date.getSeconds()).slice(-2)
-                                                                                            ];
-                                            //대댓글 댓글 밑에 보여주기
-                                                let rereply = `
-                                                    <div class="thisrereply">
-                                                        <div class="rereply">
-                                                            <div class="rereply_content">
-                                                                <p class="content_rereply">${doc.data().content}</p>
-                                                                <p class="writer_rereply">${doc.data().writer}</p>
-                                                                <p class="date_rereply">${year}-${month}-${day} ${hour}:${min}:${sec}</p>
-                                                            </div>
-                                                        </div>
-
-                                                    </div>`;
-                                                const rereply_area = document.querySelector('.rereply_area')
-                                                rereply_area.insertAdjacentHTML('beforeend', rereply)
-                                        
-                                            /* // --- 대댓글 삭제 버튼 ---
-                                                if (doc.data().uid == user.uid) {
-                                                    const btnDeleteRereply = `<button class="btnDeleteRereply" data-id="${doc.id}">삭제</button>`
-                                                    const rereply_areas = document.querySelectorAll('div.rereply')
-                                                    rereply_areas.forEach((btnDeleteArea)=>{
-                                                        btnDeleteArea.insertAdjacentHTML('afterend', btnDeleteRereply)
-                                                    })
-                                                    $(".btnDeleteRereply").click(function() {
-                                                        let docId = $(this).data('id');
-                                                        const thisrereply = db.collection('board').doc(mainPostId).collection('replies').doc(replyDocId).collection('rereplies').doc(docId)
-                                                        console.log(thisrereply)
-                                                        if (confirm('삭제하시겠습니까?')) {
-                                                            thisrereply.delete().then(() => {
-                                                                alert('삭제되었습니다.')
-                                                                location.reload(true);
-                                                            }).catch((error) => {
-                                                                console.log('댓글 삭제 중 에러가 발생했습니다:', error);
-                                                                alert('댓글 삭제 중 에러가 발생했습니다.')
-                                                            });
-                                                        }
-                                                    })
-                                                }  */
-                                            })
-                                        }
-                                    })
-                                        
-                                }
-                                checkRereplies()
-
-                                $("button.rereply").click(function() {
-                                    const replyId = $(this).data('id');
-                                    const content = $("textarea.rereplyInput").val();
-                                    const currentUser = firebase.auth().currentUser;
-                                    const userName = currentUser.displayName;
-                                    const userID = currentUser.uid;
-                                    const rereplyPost = {
-                                        content: content,
-                                        date: new Date(),
-                                        writer: userName,
-                                        uid: userID
-                                    };
-                                    console.log(replyId, content, rereplyPost)
-                                    function uploadRereply() {
-                                        const rerepliesRef = db.collection('board').doc(queryString.get('id')).collection('replies').doc(replyId).collection('rereplies');
-                                        rerepliesRef.add(rereplyPost).then(() => {
+                                $(".btnDeleteReply").click(function() {
+                                    let docId = $(this).data('id');
+                    
+                                    if (confirm('삭제하시겠습니까?')) {
+                                        db.collection('board').doc(queryString.get('id')).collection('replies').doc(docId).delete().then(() => {
+                                            alert('삭제되었습니다.')
                                             location.reload(true);
                                         }).catch((error) => {
-                                            alert('에러가 발생했습니다.')
-                                            console.log(error)
-                                        })
+                                            console.log('게시글 삭제 중 에러가 발생했습니다:', error);
+                                            alert('게시글 삭제 중 에러가 발생했습니다.')
+                                        });
                                     }
-                                    uploadRereply();
                                 })
-                            })
-                        }
-                    })
-                }
-                checkReplies()
-
-                $('.delete').on('click', function() {
-                    let docId = $(this).data('id');
-        
-                    if (confirm('삭제하시겠습니까?')) {
-                        db.collection('board').doc(docId).delete().then(()=>{
-                            alert('삭제되었습니다.')
-                            window.location.href = 'board.html'
-                        }).catch((error) => {
-                            console.log('게시글 삭제 중 에러가 발생했습니다:', error);
-                            alert('게시글 삭제 중 에러가 발생했습니다.')
+                            }
                         });
                     }
                 })
-                
-                $("#btnReply").click(function() {
-                    const content = $("div.replyInput > textarea").val();
+                }
+                checkReplies()
+
+            $('.delete').on('click', function() {
+                let docId = $(this).data('id');
+
+                if (confirm('삭제하시겠습니까?')) {
+                    db.collection('board').doc(docId).delete().then(()=>{
+                        alert('삭제되었습니다.')
+                        window.location.href = 'board.html'
+                    }).catch((error) => {
+                        console.log('게시글 삭제 중 에러가 발생했습니다:', error);
+                        alert('게시글 삭제 중 에러가 발생했습니다.')
+                    });
+                }
+            })
+
+            $("#btnReply").click(function() {
+                const content = $("div.replyInput > textarea").val();
                     const currentUser = firebase.auth().currentUser;
                     const userName = currentUser.displayName;
                     const userID = currentUser.uid;
@@ -253,17 +175,16 @@ firebase.auth().onAuthStateChanged(function(user) {
                         })
                     }
                     uploadReply();
-        
-                })
-        }).catch((error)=>{
+
+            })
+        }).catch((error) => {
             console.log(error)
-            alert('에러')
+            alert('에러가 발생했습니다.')
         })
-        
     } else {
-            $('div.wrapper').empty();
-            let signIn = "<div class='alert'><p><a href='login.html'>로그인 후 이용해주세요.</a></p></div>"
-            $('div.wrapper').append(signIn);
+        $('div.wrapper').empty();
+        let signIn = "<div class='alert'><p><a href='login.html'>로그인 후 이용해주세요.</a></p></div>"
+        $('div.wrapper').append(signIn);
     }
 })
 
