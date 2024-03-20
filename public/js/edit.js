5f877e09a41549c4c43a9d00326a3b770f745ddc
유안 <euantheuan@google.com>
2024-03-20 15:25:00 +0900

    file edit error fixed

--- public/js/edit.js
+++ public/js/edit.js
@@ -12,11 +12,38 @@ $('#modify').click(function() {
         title: $('#title').val(),
         content: $('#content').val()
     };
-    db.collection('board').doc(postId).update(changed).then(() => {
-        alert('수정되었습니다.')
-        window.location.href = `/detail.html?id=${postId}`
-    }).catch((error) => {
-        console.log(error)
-        alert('에러가 발생했습니다.')
-    })
+
+    function updateToFirestore(changed) {
+        db.collection('board').doc(postId).update(changed).then(() => {
+            alert('수정되었습니다.')
+            window.location.href = `/detail.html?id=${postId}`
+        }).catch((error) => {
+            console.log(error)
+            alert('에러가 발생했습니다.')
+        })
+    }
+
+    if (confirm('수정하시겠습니까?')) {
+        if (!document.querySelector('#thumbnail').files[0]) {
+            updateToFirestore(changed)
+        } else {
+            const file = document.querySelector('#thumbnail').files[0];
+            const storageRef = storage.ref();
+            const storagePath = storageRef.child('img/' + file.name);
+            const uploadImg = storagePath.put(file);
+    
+            uploadImg.on('state_changed', null, (error) => {
+                console.log(error);
+                alert('에러가 발생했습니다.');
+            }, () => {
+            uploadImg.snapshot.ref.getDownloadURL()
+                .then((url) => {
+                        changed.image = url;
+                        updateToFirestore(changed);
+                });
+            });
+        }
+    }
+    
+    
 })
--- public/js/edithari.js
+++ public/js/edithari.js
@@ -1,4 +1,5 @@
 const db = firebase.firestore();
+const storage = firebase.storage();
 let queryString = new URLSearchParams(window.location.search)
 let postId = queryString.get('id')
 
@@ -13,29 +14,35 @@ $('#modify').click(function() {
           content: $('#content').val()
      };
 
+     function updateToFirestore(changed) {
+          db.collection('hari').doc(postId).update(changed).then(() => {
+               alert('수정되었습니다.')
+               window.location.href = `/detail.html?id=${postId}`
+          }).catch((error) => {
+               console.log(error)
+               alert('에러가 발생했습니다.')
+          })
+     }
+
      if (confirm('수정하시겠습니까?')) {
           if (!document.querySelector('#thumbnail').files[0]) {
-               db.collection('hari').doc(postId).update(changed).then(() => {
-                    alert('수정되었습니다.')
-                    windogit w.location.href = `/detail.html?id=${postId}`
-               }).catch((error) => {
-                    console.log(error)
-                    alert('에러가 발생했습니다.')
-               })
+               updateToFirestore(changed)
           } else {
                const file = document.querySelector('#thumbnail').files[0];
                const storageRef = storage.ref();
                const storagePath = storageRef.child('img/' + file.name);
                const uploadImg = storagePath.put(file);
      
-               changed.img = uploadImg
-     
-               db.collection('hari').doc(postId).update(changed).then(()=>{
-     
-               }).catch((error) => {
-                    console.log(error)
-                    alert('에러가 발생했습니다.')
-               })
+               uploadImg.on('state_changed', null, (error) => {
+                    console.log(error);
+                    alert('에러가 발생했습니다.');
+               }, () => {
+               uploadImg.snapshot.ref.getDownloadURL()
+                    .then((url) => {
+                         changed.image = url;
+                         updateToFirestore(changed);
+                    });
+               });
           }
      }
      
